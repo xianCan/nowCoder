10350cfa9a3656f92b6751bd641eb3c5009785fb
xianCan <2492716147@qq.com>
2019-01-03 14:54:09 +0800

    冒泡排序

--- src/nowCoder/basicClass1/BubbleSort.java
+++ src/nowCoder/basicClass1/BubbleSort.java
@@ -5,4 +5,25 @@ package nowCoder.basicClass1;
  * @date 2019/1/3 0:07
  */
 public class BubbleSort {
+    /**
+     * 冒泡排序，时间复杂度O(N^2)，额外空间复杂度O(1)
+     * 工程上已基本不使用
+     * @param arr
+     */
+    public static void bubbleSort(int[] arr){
+        if (arr == null || arr.length<2) return;
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;j<arr.length-i-1;j++){
+                if (arr[j]>arr[j+1]){
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
