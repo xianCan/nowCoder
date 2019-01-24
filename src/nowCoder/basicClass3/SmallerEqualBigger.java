7b81731ab03cb026439837fde5a4c141ae24a61a
xianCan <2492716147@qq.com>
2019-01-24 09:13:53 +0800

    将单向链表按某值划分成左边小、中间相等、右边大的形式（未完成）

---
+++ src/nowCoder/basicClass3/SmallerEqualBigger.java
@@ -0,0 +1,23 @@
+package nowCoder.basicClass3;
+
+/**
+ * @authod xianCan
+ * @date 2019/1/11 17:26
+ */
+public class SmallerEqualBigger {
+    public static Node smallerEqualBigger(Node head, int pivot){
+
+        return null;
+    }
+
+    /**
+     * 单链表
+     */
+    public static class Node{
+        public int value;
+        public Node next;
+        public Node(int data){
+            this.value = data;
+        }
+    }
+}
